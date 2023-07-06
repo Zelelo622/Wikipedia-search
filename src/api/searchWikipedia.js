d7db2d4e9c847e349121a193de15113a5de8cc04
Egor Kirin <egorcom27@gmail.com>
2023-07-06 16:20:17 +0300

    адаптив

--- src/api/searchWikipedia.js
+++ src/api/searchWikipedia.js
@@ -5,7 +5,6 @@ export const searchWikipedia = async (searchStore) => {
   const endpoint = `https://ru.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=40&srsearch=${searchStore.searchQuery}`;
   axios.get(endpoint).then((res) => {
     const persons = res.data;
-    console.log(persons);
     searchStore.setSearchResult(persons.query.search);
     searchStore.setSearchInfo(persons.query.searchinfo);
   });
--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -7,26 +7,30 @@ const SearchResult = observer(({ searchResult, searchInfo }) => {
       <p className="search__result-total">
         {searchInfo.totalhits ? "Search Result: " + searchInfo.totalhits : ""}
       </p>
-      {searchResult.map((result, i) => {
-        const url = `https://ru.wikipedia.org/?curid=${result.pageid}`;
-        return (
-          <a
-            href={url}
-            target="_blank"
-            rel="noreferrer"
-            className="search__result-link"
-            key={i}
-          >
-            <div className="search__result-wrapper">
-              <h3 className="search__result-title">{result.title}</h3>
-              <p
-                className="search__result-text"
-                dangerouslySetInnerHTML={{ __html: result.snippet }}
-              ></p>
-            </div>
-          </a>
-        );
-      })}
+      {searchResult.length === 0 ? (
+        <h3 className="search__result-title">Ничего не найдено</h3>
+      ) : (
+        searchResult.map((result, i) => {
+          const url = `https://ru.wikipedia.org/?curid=${result.pageid}`;
+          return (
+            <a
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+              className="search__result-link"
+              key={i}
+            >
+              <div className="search__result-wrapper">
+                <h3 className="search__result-title">{result.title}</h3>
+                <p
+                  className="search__result-text"
+                  dangerouslySetInnerHTML={{ __html: result.snippet }}
+                ></p>
+              </div>
+            </a>
+          );
+        })
+      )}
     </div>
   );
 });
